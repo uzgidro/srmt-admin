f1d13d839d49e7e87434dde6dab47756a261bdf6
suxa56 <suxa5600@gmail.com>
2025-06-11 16:29:32 +0500

    logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"srmt-admin/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Printf("%#v\n", cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info(cfg.Address)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
